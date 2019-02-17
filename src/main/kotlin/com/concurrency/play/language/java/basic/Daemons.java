2a486b028f579781e7f8e3aa7a02ca0ea322330b
parkito <artyom-karnov@yandex.ru>
2019-02-17 16:18:00 +0300

    Add daemos example

---
+++ src/main/kotlin/com/concurrency/play/language/java/basic/Daemons.java
@@ -0,0 +1,14 @@
+package com.concurrency.play.language.java.basic;
+
+public class Daemons {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new DaemonThread(5));
+        t1.start();
+
+        Thread t2 = new Thread(new DaemonThread(5));
+        t2.setDaemon(true);
+        t2.start();
+
+        System.out.println("Main thread is finished");
+    }
+}
