7eda8076eba41dbc4261eb638c92c3f7ce9f1ad6
parkito <artyom-karnov@yandex.ru>
2021-01-28 21:49:47 -0700

    Add flux creation examples

---
+++ src/main/kotlin/com/concurrency/play/reactive/01_Basiscs.kt
@@ -0,0 +1,80 @@
+package com.concurrency.play.reactive
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.reactivestreams.FlowAdapters
+import reactor.adapter.JdkFlowAdapter
+import reactor.core.publisher.Flux
+import reactor.core.publisher.FluxSink
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.util.Date
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.Supplier
+import java.util.stream.Stream
+
+class Basics {
+
+    val executorService = Executors.newFixedThreadPool(1)
+
+    @Test
+    fun createPublishers() {
+        val now = System.currentTimeMillis()
+        val greetingMono = Mono.just(Date(now))
+        StepVerifier.create(greetingMono).expectNext(Date(now)).verifyComplete()
+
+        val letters = Flux.just("A", "B", "C")
+        StepVerifier.create(letters).expectNext("A", "B", "C").verifyComplete()
+
+        val integer = AtomicInteger()
+        val supplier = Supplier { integer.incrementAndGet() }
+        val integerFlux = Flux.fromStream(Stream.generate(supplier))
+        StepVerifier.create(integerFlux.take(3)).expectNext(1).expectNext(2).expectNext(3)
+            .verifyComplete()
+    }
+
+    @Test
+    fun jdkFlowConversion() {
+        val original = Flux.range(0, 10)
+
+        val rangeOfIntegersAsJdk9Flow = FlowAdapters
+            .toFlowPublisher(original)
+        val rangeOfIntegersAsReactiveStream = FlowAdapters
+            .toPublisher(rangeOfIntegersAsJdk9Flow)
+
+        StepVerifier.create(original).expectNextCount(10).verifyComplete()
+
+        StepVerifier.create(rangeOfIntegersAsReactiveStream).expectNextCount(10)
+            .verifyComplete()
+
+        val rangeOfIntegersAsReactorFluxAgain = JdkFlowAdapter
+            .flowPublisherToFlux(rangeOfIntegersAsJdk9Flow)
+        StepVerifier.create(rangeOfIntegersAsReactorFluxAgain).expectNextCount(10)
+            .verifyComplete()
+    }
+
+    @Test
+    fun createFluxFromOuterWorld() {
+
+        val integers = Flux.create { emitter: FluxSink<Int> -> launch(emitter, 5) }
+        StepVerifier
+            .create(integers.doFinally { executorService.shutdown() })
+            .expectNextCount(5) //
+            .verifyComplete()
+
+    }
+
+    private fun launch(integerFluxSink: FluxSink<Int>, count: Int) {
+        executorService.submit {
+            val integer = AtomicInteger()
+            Assertions.assertNotNull(integerFluxSink)
+            while (integer.get() < count) {
+                val random = Math.random()
+                integerFluxSink.next(integer.incrementAndGet())
+                Thread.sleep((random * 1000).toLong())
+            }
+            integerFluxSink.complete()
+        }
+    }
+}
